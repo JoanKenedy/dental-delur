8012269c712ba5a9de78e51d9571109d79fe20c1
Joan Kenedy <joankenedi9311@gmail.com>
2025-03-12 16:50:59 -0600

    promos-marzo-2025

--- src/components/Promociones/Promocion.jsx
+++ src/components/Promociones/Promocion.jsx
@@ -1,12 +1,9 @@
-import Promo1 from "../../assets/promo-4.jpg";
 import Promo2 from "../../assets/promo-2.jpg";
 import Promo3 from "../../assets/promo-3.jpg";
 import Promo4 from "../../assets/promo-1.jpg";
-import Promo5 from "../../assets/promo-5.jpg";
-import Promo6 from "../../assets/promo-6.jpg";
 
 export const Promocion = () => {
-  const imagenesPromo = [Promo1, Promo2, Promo3, Promo4, Promo5, Promo6];
+  const imagenesPromo = [Promo2, Promo3, Promo4];
 
   return (
     <>
